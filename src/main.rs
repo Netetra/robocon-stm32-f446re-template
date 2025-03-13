6e5422cb3daffbf7353de903efc24e2e488f2a29
Netetra <netetra07@gmail.com>
2025-03-13 22:08:58 +0900

    feat: pingに応答する処理を追加

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,19 @@ use panic_probe as _;
 mod app {
     use bxcan::{filter::Mask32, Data, Frame, Id, Rx0, Rx1, StandardId, Tx};
     use defmt::*;
+    use roboken_rs::can::id::{id_build, id_parse, Endpoint, NodeId};
     use stm32f4xx_hal::{
         can::Can,
         gpio::{Output, Pin},
         pac::{CAN1, TIM7},
         prelude::*,
-        rcc::RccExt,
         timer::{Counter, Event},
     };
     use systick_monotonic::{ExtU64, Systick};
 
     const TICK_INTERVAL_MS: u32 = 1000;
     const LED_BLINK_TIME_MS: u64 = 25;
+    const NODE_ID: u8 = 0b_0001;
 
     #[monotonic(binds = SysTick, default = true)]
     type Tonic = Systick<1_000>;
@@ -59,7 +60,7 @@ mod app {
             .leave_disabled();
         let mut filters = bxcan.modify_filters();
         let mut mask = Mask32::frames_with_std_id(
-            StandardId::new(0b_0000_0000_000).unwrap(),
+            StandardId::new((NODE_ID << 3) as u16).unwrap(),
             StandardId::new(0b_0000_1111_000).unwrap(),
         );
         filters.enable_bank(0, bxcan::Fifo::Fifo0, *mask.data_frames_only());
@@ -105,13 +106,36 @@ mod app {
     }
 
     #[task]
-    fn data_frame_handle(_: data_frame_handle::Context, id: StandardId, data: Data) {
-        // do something.
+    fn pong(_: pong::Context, origin: NodeId) {
+        let id = StandardId::new(id_build(
+            NodeId::from_raw(NODE_ID).unwrap(),
+            origin,
+            Endpoint::Zero,
+        ))
+        .unwrap();
+        let frame = Frame::new_data(id, [112, 111, 110, 103]); // pong.
+        transmit::spawn(frame).unwrap();
     }
 
     #[task]
-    fn remote_frame_handle(_: remote_frame_handle::Context, id: StandardId) {
-        // do something.
+    fn data_frame_handle(_: data_frame_handle::Context, id: StandardId, data: Data) {
+        let (origin, _, endpoint) = id_parse(id.as_raw());
+        match endpoint {
+            Endpoint::Zero => {
+                if data == Data::new(&[112, 105, 110, 103]).unwrap() {
+                    pong::spawn(origin).unwrap();
+                }
+            }
+            _ => {}
+        }
+    }
+
+    #[task]
+    fn remote_frame_handle(_: remote_frame_handle::Context, id: StandardId, _dlc: u8) {
+        let (_origin, _, endpoint) = id_parse(id.as_raw());
+        match endpoint {
+            _ => {}
+        }
     }
 
     #[task(local=[tx], shared=[tx_led])]
@@ -142,7 +166,7 @@ mod app {
             let _ = rx_led_off::spawn_after(LED_BLINK_TIME_MS.millis());
             let recv = nb::block!(ctx.local.rx1.receive()).unwrap();
             if let Id::Standard(id) = recv.id() {
-                remote_frame_handle::spawn(id).unwrap();
+                remote_frame_handle::spawn(id, recv.dlc()).unwrap();
             }
         });
     }
